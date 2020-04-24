54d06a86733577cfe8bf587a0f75a86b4ede9f36
Scott Clampet <scottclampet@Scotts-MacBook-Pro.local>
2020-04-23 19:02:21 -0600

    initial project creation

---
+++ Movieflix/Movieflix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Movieflix
+//
+//  Created by Scott Clampet on 4/23/20.
+//  Copyright © 2020 Scott Clampet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
